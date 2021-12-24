ba97750ce0dc28706f5423bd4d2583241b44f244
kjkrupal <kjkrupal@gmail.com>
2021-12-24 18:02:35 -0500

    refactor project

---
+++ kubeio-wineservice/src/main/java/com/kubeio/wines/WineServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kubeio.wines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WineServiceApplication.class, args);
+	}
+
+}
