8a81497d26bd3cdc5c983d07ab76dd91a8529645
kjkrupal <kjkrupal@gmail.com>
2021-12-24 20:05:01 -0500

    added caching

--- kubeio-wineservice/src/main/java/com/kubeio/wines/WineServiceApplication.java
+++ kubeio-wineservice/src/main/java/com/kubeio/wines/WineServiceApplication.java
@@ -2,8 +2,10 @@ package com.kubeio.wines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WineServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WineServiceApplication.class, args);
