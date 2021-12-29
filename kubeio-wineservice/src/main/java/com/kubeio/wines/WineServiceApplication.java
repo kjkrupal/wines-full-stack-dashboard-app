855678412f20eeaae5774d5e5d7eee9efea3eb11
kjkrupal <kjkrupal@gmail.com>
2021-12-29 15:35:26 -0500

    add eureka client annotation

--- kubeio-wineservice/src/main/java/com/kubeio/wines/WineServiceApplication.java
+++ kubeio-wineservice/src/main/java/com/kubeio/wines/WineServiceApplication.java
@@ -3,9 +3,11 @@ package com.kubeio.wines;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaClient
 public class WineServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WineServiceApplication.class, args);
