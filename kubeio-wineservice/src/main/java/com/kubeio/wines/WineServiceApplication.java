ff9cd747ae7d0595f3c15ba9d82f52afae855a42
kjkrupal <kjkrupal@gmail.com>
2021-12-30 12:15:45 -0500

    FINALLY

--- kubeio-wineservice/src/main/java/com/kubeio/wines/WineServiceApplication.java
+++ kubeio-wineservice/src/main/java/com/kubeio/wines/WineServiceApplication.java
@@ -3,7 +3,10 @@ package com.kubeio.wines;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching
@@ -13,4 +16,9 @@ public class WineServiceApplication {
 		SpringApplication.run(WineServiceApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
